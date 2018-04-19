d4dab0cca3a41565c524e2a885c3c12e9f0eb79f
Kumar Miskin <kumarpmiskin@kumars-macbook-pro.local>
2018-04-19 13:40:38 +0530

    Initial Commit

---
+++ Killer_Sudoku/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Killer_Sudoku
+//
+//  Created by Kumar P Miskin on 19/04/18.
+//  Copyright © 2018 Kumar P Miskin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
